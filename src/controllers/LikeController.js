6ce1c612000363e5e74c49cfebdf03015aadf07e
Carlos <ce-sarr@hotmail.com>
2019-08-12 23:01:35 -0300

    Implementando e estilizando ação de Match em tempo teal

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -7,11 +7,18 @@ module.exports = {
         const { devId } = req.params;
 
         const loggedDev = await Dev.findById(user);
-        
         const targetDev = await Dev.findById(devId);
        
         if (targetDev.likes.includes(loggedDev._id)) {
-            console.log('deu match');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         };
         loggedDev.likes.push(targetDev._id);
 
